6e23c3ac5fb03a2f68402ecff13976dee7a52149
Pandabearingstreet <38286947+Pandabearingstreet@users.noreply.github.com>
2025-12-05 15:49:02 +0100

    solved day 4

---
+++ day4/solverpart1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy.signal as signal
+with open('day4/input.txt') as f:
+    data = f.read().split('\n')
+# make the data a numpy maxtrix, replacing @ with 1 and . with 0
+data = [x.replace('@', '1') for x in data]
+data = [x.replace('.', '0') for x in data]
+data = [list(x) for x in data]
+data = np.array(data, dtype=int)
+
+# convolve with a 3x3 uniform kernel
+neighbors = signal.convolve2d(data, np.ones((3,3)), mode= 'same', boundary='fill', fillvalue=0)
+# the relevant condition is where in the data, there is a 1, and in the neighbors there is a 4 or lower
+data = data * (neighbors <= 4)
+
+
+print(np.sum(data))
